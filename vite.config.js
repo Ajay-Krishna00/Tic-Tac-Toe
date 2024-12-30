8210c0e6378d0caf2b5e6cb2b95350815d14cd14
Ajay-Krishna00 <enthusiastajay00@gmail.com>
2024-12-30 21:47:35 +0530

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Tic-Tac-Toe/", // Set to the name of your repository
   plugins: [react()],
 });
